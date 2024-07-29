129ea3a5f3a2da137a7ad64de948d96d5133e509
onlineresultsite <footflux@gmail.com>
2024-07-29 16:05:38 +0530

    del

--- webempresa/services/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import services
-
-
-# Create your views here.
-def service(request):
-    servicios = services.objects.all()
-    return render(request, "services/services.html", {'servicios': servicios})
